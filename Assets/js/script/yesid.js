1a19c0374e978cb8e32015457904d0b2fedab1c3
Yessid Acosta <yesid9523@gmail.com>
2022-10-20 21:47:44 -0500

    Seccion clientes
    Seccion clientes parcialmente terminada

--- Assets/js/script/yesid.js
+++ Assets/js/script/yesid.js
@@ -59,6 +59,37 @@ function frmRegistrarCliente(e) {
     }
 }
 
+function frmActualizarCliente(e) {
+    e.preventDefault();
+    const url = base_url + "Clientes/actualizarCliente";
+    const frm = document.getElementById("frmCliente");
+    const http = new XMLHttpRequest();
+    http.open("POST", url, true);
+    http.send(new FormData(frm));
+    http.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200) {
+            const res = this.responseText;
+            console.log(res);
+            if (res == "ok") {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Cliente actualizado con exito',
+                    showConfirmButton: false,
+                    timer: 3000
+                })
+                frm.reset();
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: res,
+                    showConfirmButton: false,
+                    timer: 3000
+                })
+            }
+        }
+    }
+}
+
 $(document).ready(function(e){
     $("#departamento").change(function () {
         var iddepto = $("#departamento").val();
@@ -74,3 +105,4 @@ $(document).ready(function(e){
         });
     })
 });
+
--- Views/Clientes/editClientes.php
+++ Views/Clientes/editClientes.php
@@ -1,5 +1,91 @@
 <?php include "Views/Templates/header.php"; ?>
 
-    <div>editar clientes</div>
+<form method="post" id="frmCliente">
+    <!-- container datos personales -->
+    <div class="card mb-3">
+        <div class="card-body">
+            <div class="row">
+                <h5 class="mb-3">Datos personales</h5>
+            </div>
+            <div class="row">
+                <div class="col-xl-3 col-sm-4">
+                    <div class="mb-3">
+                        <label for="cedula" class="form-label">Cedula</label>
+                        <input type="text" class="form-control" name="cedula" id="cedula" value="<?php echo $data['cedula']; ?>" placeholder="Cedula" readonly>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-sm-4">
+                    <div class="mb-3">
+                        <label for="nombres" class="form-label">Nombres</label>
+                        <input type="text" class="form-control" name="nombres" id="nombres" value="<?php echo $data['nombres']; ?>" placeholder="Nombres">
+                    </div>
+                </div>
+                <div class="col-xl-3 col-sm-4">
+                    <div class="mb-3">
+                        <label for="apellidos" class="form-label">Apellidos</label>
+                        <input type="text" class="form-control" name="apellidos" id="apellidos" value="<?php echo $data['apellidos']; ?>" placeholder="Apellidos">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                    <div class="col-xl-3 col-sm-4">
+                        <div class="mb-3">
+                            <label for="direccion" class="form-label">Direccion</label>
+                            <input type="text" class="form-control" name="direccion" id="direccion" value="<?php echo $data['direccion']; ?>" placeholder="Direccion">
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-sm-4">
+                        <div class="mb-3">
+                            <label for="telefono" class="form-label">Telefono</label>
+                            <input type="text" class="form-control" name="telefono" id="telefono" value="<?php echo $data['telefono']; ?>" placeholder="Telefono">
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-sm-4">
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="text" class="form-control" name="email" id="email" value="<?php echo $data['email']; ?>" placeholder="Email">
+                        </div>
+                    </div>
+                </div>
+            <div class="row">
+                <div class="col-xl-3 col-sm-4">
+                    <div class="mb-3">
+                        <label for="departamento" class="form-label">Departamento</label>
+                        <select class="form-select" aria-label="Default select example" name="departamento" id="departamento">
+                            <option value="<?php echo $data['iddepto']; ?>" selected hidden><?php echo $data['departamento']; ?></option>
+                            <?php foreach ($data['departamentos'] as $row) { ?>
+                                <option value="<?php echo $row['iddepto']; ?>"><?php echo $row['departamento']; ?></option>
+                            <?php } ?>
+                            
+                        </select>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-sm-4">
+                    <div class="mb-3">
+                        <label for="municipio" class="form-label">Municipio</label>
+                        <select class="form-select" aria-label="Default select example" name="municipio" id="municipio">
+                            <option value="<?php echo $data['idmunicipio']; ?>" selected hidden><?php echo $data['municipio']; ?></option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-sm-4">
+                    <div class="mb-3">
+                        <label for="fechanac" class="form-label">Fecha Nacimiento</label>
+                        <input class="form-control border" id="fechanac" name="fechanac" type="date" value="<?php echo $data['fechanac']?>" onblur="// vlfecha()" placeholder="Fecha nacimiento" />
+                    </div>
+                </div>
+                <div class="row d-flex justify-content-center">
+                    <div class="col-3 d-flex justify-content-center">
+                        <button class="btn btn-primary" type="button" onclick="frmActualizarCliente(event);">Registrar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+
+
+
+
 
 <?php include "Views/Templates/footer.php"; ?>
